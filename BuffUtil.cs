d3155f1db4f26a242570cda2818e578ec26f64a7
dxbydt <dxbydt@gmail.com>
2021-04-17 17:49:32 -0700

    Fixing after buff change

--- BuffUtil.cs
+++ BuffUtil.cs
@@ -436,7 +436,7 @@ namespace BuffUtil
                 if (isDead)
                     return false;
 
-                buffs = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Life>().Buffs;
+                buffs = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<ExileCore.PoEMemory.Components.Buffs>().BuffsList;
                 if (buffs == null)
                     return false;
 
